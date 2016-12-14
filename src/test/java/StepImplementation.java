b73fecb72bbfa05a61fb95d9032d2d9170796ac3
balaindin <balaindin@yahoo.com>
2016-12-13 21:48:28 -0500

    Added class level comment

--- src/test/java/StepImplementation.java
+++ src/test/java/StepImplementation.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Gauge implementaiton
+ */
 public class StepImplementation {
 
     private HashSet<Character> vowels;
